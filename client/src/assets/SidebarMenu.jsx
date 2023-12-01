a87dbef572d1778f9d56728647c73e9bc8ce3642
Guilherme Baia <guilhermebaiav@gmail.com>
2023-11-30 21:26:44 -0300

    + sidebar menu, fixing table structure

---
+++ client/src/assets/SidebarMenu.jsx
@@ -0,0 +1,13 @@
+export function SidebarMenu(){
+    return (
+        <nav className="h-screen w-72 bg-stone-900">
+            <div className="border-b border-white/20 px-5 py-10 text-white">
+                <p className="font-bold">PAINEL DE CONTROLE</p>
+                <p className="text-sm">Usuário: Carlos Teixeira</p>
+            </div>
+            <a href="#" className="w-full block px-5 py-5 border-b border-white/20 text-white hover:bg-white/10">Painel Usuários</a>
+         
+        </nav>
+    )
+}
+
--- client/src/assets/UserPanel.jsx
+++ client/src/assets/UserPanel.jsx
@@ -1,12 +1,16 @@
+import { SidebarMenu } from "./SidebarMenu";
+
 export function USerPanel(){
     return(
-        <section>
+        <section className="flex">
+            <SidebarMenu/>
             <main className="w-full min-h-screen px-32 pt-36">
                 <div className="flex justify-between items-center mb-5">
                 <Title>Usuários</Title>
                 <FilledButton>+ Novo Usuário</FilledButton>
                 </div>
                 <table className="table-auto w-full text-left text-stone-500">
+                    <thead>
                     <tr className="bg-stone-900 text-white border-x border-stone-900">
                         <th className="py-5 px-5">ID</th>
                         <th>Nome</th>
@@ -14,12 +18,15 @@ export function USerPanel(){
                         <th>Opções</th>
                 
                     </tr>
+                    </thead>
+                    <tbody>
                     <tr className=" border-b border-x border-slate-300 hover:text-stone-900">
                         <td className="py-6 px-5">12345</td>
                         <td>Carlos Eduardo Teixeira</td>
                         <td>Aluno</td>
                         <td>X Edit</td>
                     </tr>
+                    </tbody>
                 
                 </table>
             </main>
